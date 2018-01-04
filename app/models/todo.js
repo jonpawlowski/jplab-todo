d71a6c28872684d9697237570da9a588213a4308
jonpawlowski <jon.pawlowski@gmail.com>
2018-01-04 15:41:12 -0800

    Format page

--- app/models/todo.js
+++ app/models/todo.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Todo', {
     title: String,
 	description: String,
-	start_date: { type: Date, default: Date.now },
-	completed_date: { type: Date, default: '' },
+	//start_date: { type: Date, default: Date.now },
+	//completed_date: { type: Date, default: '' },
+	start_date: String,
+	completed_date: String,
 	priority: Number
 });
