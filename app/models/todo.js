716adf002c80c196d08d6e6fa4352436622e5b40
root <root@dev-149.jplab.com>
2018-01-05 00:07:12 -0800

    Updated model

--- app/models/todo.js
+++ app/models/todo.js
@@ -2,10 +2,8 @@ var mongoose = require('mongoose');
 
 module.exports = mongoose.model('Todo', {
     title: String,
-	description: String,
-	//start_date: { type: Date, default: Date.now },
-	//completed_date: { type: Date, default: '' },
-	start_date: String,
-	completed_date: String,
-	priority: Number
+    description: String,
+    start_date: String,
+    completed_date: String,
+    priority: Number
 });
