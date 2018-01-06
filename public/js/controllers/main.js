bdc763370959f528577ab096bcfccf9b65f7cd23
jonpawlowski <jon.pawlowski@gmail.com>
2018-01-05 20:36:44 -0800

    Add notifications

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -29,7 +29,7 @@ angular.module('todoController', [])
 					// if successful creation, call our get function to get all the new todos
 					.success(function(data) {
 						$scope.loading = false;
-						
+						$scope.pop();
 						$scope.formData = {}; // clear the form so our user is ready to enter another
 						$scope.todos = data; // assign our new list of todos
 					});
@@ -51,6 +51,6 @@ angular.module('todoController', [])
 		
 		// Display toaster message
 		$scope.pop = function() {
-			toaster.pop('success', "Success", "To Do Item Added");
+			toaster.pop('success', "Success", "To Do Item Added", 5000);
 		}
 	}]);
