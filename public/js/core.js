85201b55bce33a0d97d6d88fc9d6c8a7e8843ad3
jonpawlowski <jon.pawlowski@gmail.com>
2018-04-27 15:37:42 -0700

    adding progress logic for already completed items

--- public/js/core.js
+++ public/js/core.js
@@ -1,1 +1,1 @@
-angular.module('jplabTodo', ['todoController', 'todoService','720kb.datepicker','toaster','xeditable']);
+angular.module('jplabTodo', ['todoController', 'todoService','720kb.datepicker','toaster', ['xeditable']);
